4cf1fd05b65abca012389af617cc70a2002b7c4f
shirocodes <ishirapt@gmail.com>
2025-05-14 07:59:51 +0300

    Completed the lab.

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,16 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+# Create a new instance of Vehicle
+my_vehicle = Car("medium", 4)
+
+# Test the attributes
+print(f"Wheel size: {my_vehicle.wheel_size}")
+print(f"Wheel number: {my_vehicle.wheel_number}")
+
+# Test the methods
+print(my_vehicle.go())
+print(my_vehicle.fill_up_tank())
